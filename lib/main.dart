3b3dade063ace5f5b00e99fb264d2f4e10d19a32
udasitharani <udasitharani@gmail.com>
2020-08-30 14:51:27 +0530

    bottom nav bar initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,78 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeState extends State<Home> {
+  int _index = 0;
+
+  void _setIndex(index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar:
-          BottomNavigationBar(items: <BottomNavigationBarItem>[]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _index == 0 ? Colors.white : Colors.black,
+              size: 25.0,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: _index == 0 ? Colors.white : Colors.black,
+              size: 25.0,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add,
+              color: _index == 0 ? Colors.white : Colors.black,
+              size: 25.0,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.message_outlined,
+              color: _index == 0 ? Colors.white : Colors.black,
+              size: 25.0,
+            ),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: _index == 0 ? Colors.white : Colors.black,
+              size: 25.0,
+            ),
+            title: Text(''),
+          ),
+        ],
+        currentIndex: 0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: _setIndex,
+      ),
       body: Center(),
     );
   }

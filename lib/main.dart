7bd54bdda6de2e297936724ab6a628a481fb08b5
udasitharani <udasitharani@gmail.com>
2020-08-31 10:16:36 +0530

    complete bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       title: 'TokTok',
       theme: ThemeData(
@@ -37,55 +39,135 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: _index == 0 ? Colors.white : Colors.black,
-              size: 25.0,
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: _index == 0 ? Colors.black : Colors.white,
+        ),
+        child: BottomNavigationBar(
+          elevation: 0.0,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: BottomNavBarItemIcon(
+                  index: 0,
+                  selectedIndex: _index,
+                  icon: Icon(
+                    Icons.home,
+                    color: _index == 0 ? Colors.white : Colors.black,
+                    size: 25.0,
+                  ),
+                  selectedIcon: Column(
+                    children: [
+                      Icon(
+                        Icons.home,
+                        color: _index == 0 ? Colors.white : Colors.black,
+                        size: 25.0,
+                      ),
+                      SizedBox(height: 5.0),
+                      Container(
+                        width: 25.0,
+                        height: 2.0,
+                        color: _index == 0 ? Colors.white : Colors.black,
+                      ),
+                    ],
+                  )),
+              title: Text(''),
             ),
-            title: Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.search,
-              color: _index == 0 ? Colors.white : Colors.black,
-              size: 25.0,
+            BottomNavigationBarItem(
+              icon: BottomNavBarItemIcon(
+                index: 1,
+                selectedIndex: _index,
+                icon: Icon(
+                  Icons.search_rounded,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+                selectedIcon: Icon(
+                  Icons.search,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 28.0,
+                ),
+              ),
+              title: Text(''),
             ),
-            title: Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.add,
-              color: _index == 0 ? Colors.white : Colors.black,
-              size: 25.0,
+            BottomNavigationBarItem(
+              icon: BottomNavBarItemIcon(
+                index: 2,
+                selectedIndex: _index,
+                icon: Icon(
+                  Icons.add,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+                selectedIcon: Icon(
+                  Icons.add,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+              ),
+              title: Text(''),
             ),
-            title: Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.message_outlined,
-              color: _index == 0 ? Colors.white : Colors.black,
-              size: 25.0,
+            BottomNavigationBarItem(
+              icon: BottomNavBarItemIcon(
+                index: 3,
+                selectedIndex: _index,
+                icon: Icon(
+                  Icons.message_outlined,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+                selectedIcon: Icon(
+                  Icons.message,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+              ),
+              title: Text(''),
             ),
-            title: Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: _index == 0 ? Colors.white : Colors.black,
-              size: 25.0,
+            BottomNavigationBarItem(
+              icon: BottomNavBarItemIcon(
+                index: 4,
+                selectedIndex: _index,
+                icon: Icon(
+                  Icons.person_outline,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+                selectedIcon: Icon(
+                  Icons.person,
+                  color: _index == 0 ? Colors.white : Colors.black,
+                  size: 25.0,
+                ),
+              ),
+              title: Text(''),
             ),
-            title: Text(''),
-          ),
-        ],
-        currentIndex: 0,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        onTap: _setIndex,
+          ],
+          currentIndex: 0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: _setIndex,
+        ),
       ),
       body: Center(),
     );
+  }
+}
+
+class BottomNavBarItemIcon extends StatelessWidget {
+  const BottomNavBarItemIcon({
+    Key key,
+    @required this.index,
+    @required this.selectedIndex,
+    @required this.icon,
+    @required this.selectedIcon,
+  }) : super(key: key);
+
+  final int index;
+  final int selectedIndex;
+  final Widget icon;
+  final Widget selectedIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return selectedIndex == index ? selectedIcon : icon;
   }
 }

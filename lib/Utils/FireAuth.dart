283456e497b4b52625b91e32c2ca401cdebc38b6
udasitharani <udasitharani@gmail.com>
2020-09-04 13:35:04 +0530

    initial google-signin setup

---
+++ lib/Utils/FireAuth.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FireAuth {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User> signIn() async {
+    final GoogleSignInAccount _account = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication _googleAuth =
+        await _account.authentication;
+
+    final AuthCredential _credentials = GoogleAuthProvider.credential(
+        idToken: _googleAuth.idToken, accessToken: _googleAuth.accessToken);
+
+    final User user = (await _auth.signInWithCredential(_credentials)).user;
+
+    return user;
+  }
+}
